f6942c5bd649094dd2e1e132a9366afb140d7cc2
Ferdinand Teguefouet <teguefouet1@gmail.com>
2022-06-27 22:54:34 +0200

    Modification du fichier contact

--- src/screens/Contact/Contact.js
+++ src/screens/Contact/Contact.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import Body from 'components/layout/Body/Body';
-import { useParams } from 'react-router-dom';
-
+import React from "react";
+import Body from "components/layout/Body/Body";
+import { useParams } from "react-router-dom";
 
 // Styles
 
-
 // Redux
-import {useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
 function Contact() {
-    const counterValue = useSelector((state) => state.counter.value);
-    // Id contact
-    let {id} = useParams();
+  const counterValue = useSelector((state) => state.counter.value);
+  // Id contact
+  let { id } = useParams();
 
-    return(
-            <Body title='CONTACT SCREEN'>
-                <h3>Contact : value : {counterValue}</h3>
-                <h6>Contact : id : {id}</h6>
-                
-            </Body>
-        );
+  return (
+    <Body title="CONTACT SCREEN">
+      <h3>Contact : Value : {counterValue}</h3>
+      <h6>Contact : id : {id}</h6>
+    </Body>
+  );
 }
 
 export default Contact;
